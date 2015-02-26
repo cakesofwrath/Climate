0c91ef0d144d408300bde7557d80f004c657698c
cakesofwrath <dai.alan.dai@gmail.com>
2015-02-26 04:49:41 +0000

    25 yr geojsons done.

--- data/process.js
+++ data/process.js
@@ -8,31 +8,81 @@ var data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 //console.log(data.temperature_anomaly.length);
 //console.log(data.latitude.length * data.longitude.length * data.time.length);
 //Apparently: time, then long, then lat.
+//console.log(schema.time.length/12, data.time.length/12); //165 years, 0 months
+//console.log(schema.time.length%12, data.time.length%12);
 
-/*for(var lat in schema.latitude) { //The degrees north
-    for(var long in schema.longitude) { //The degrees east.
-        for(var t in schema.time) { //days since 1850-1-1 00:00:00, continues until 2014
-            //a temperature_anomaly in deg K.
-            
-        }
+var geoJSONs = [];
+
+var dataIndex = 0;
+var t = 0, lat = 0, long = 0;
+var d = 2.5;
+
+function saveAll(gJsons) {
+    if(gJsons.length > 10){
+        console.error("Error: too many geoJSONs");
+        return;
     }
-}*/
-
-for(var t in schema.time) {
-    for(var lat in schema.latitude) {
-        for(var long in schema.longitude) {
-            
-        }
+    var yr = 1850;
+    for(var gJ in gJsons) {
+        var fileName = 'geoJSON/HadCRUT_' + yr + '.json'
+        fs.writeFile(fileName, JSON.stringify(gJsons[gJ]), function(err) {
+            if(err) throw err;
+            console.log(fileName + ' saved.');
+        });
+        yr += 25;        
     }
 }
-//algorithm for converting days since  1850-1-1 00:00:00 to actual time.
-var convertTime = function(days) {
-    var numYears =  Math.floor(days/365);
-    var numMonths = Math.floor((days - numYears * 365)/12);
-   // var numDays = Math.floor((days - numYears * 365 - numMonths * ))
-    
-    //var year = 1850 +;
-    var month = 1; //stack overflow this shiz.Should probably account for leap years.
-    var day = 1;
-    return 0;
+
+//Averages temp anomaly over a year, given an index 
+var avg_t_a = function(index) { 
+    var vals = [];
+    for(var i=index; i<index+12; i++) {
+        vals.push(parseInt(data.temperature_anomaly[i]));
+    }
+    return vals.reduce(function(a, b) { //should automatically return NaN if missing data.
+        return a + b;
+    }) / 12;
 };
+
+/*
+==============================================================================================================
+==============================================================================================================
+==============================================================================================================
+*/
+
+while(t < schema.time.length) { //The degrees north
+    var geoJSON = {
+                    'type': 'FeatureCollection',
+                    'features': []
+                };
+    while(lat < schema.latitude.length) { //The degrees east.
+        while(long < schema.longitude.length) { //days since 1850-1-1 00:00:00, continues until 2014
+            geoJSON.features.push({
+               'type': 'Feature',
+               'geometry': {
+                   'type': 'Polygon',
+                   'coordinates': [
+                        [
+                            [parseInt(data.longitude[long])+d, parseInt(data.latitude[lat])+d],
+                            [parseInt(data.longitude[long])+d, parseInt(data.latitude[lat])-d],
+                            [parseInt(data.longitude[long])-d, parseInt(data.latitude[lat])-d],
+                            [parseInt(data.longitude[long])-d, parseInt(data.latitude[lat])+d],
+                            [parseInt(data.longitude[long])+d, parseInt(data.latitude[lat])+d]
+                        ]
+                    ]
+               },
+               'properties': {
+                   'temperature_anomaly': avg_t_a(dataIndex)
+               }
+            });
+            
+            long++;
+            dataIndex++;
+        }
+        lat++;
+    }
+    t += (12 * 25);
+    geoJSONs.push(geoJSON);
+}
+
+saveAll(geoJSONs);
